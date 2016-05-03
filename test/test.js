73d8f9f1dbff887b48f8b53f43f7cd79eb96455a
Peter C <peter.chau.esq@gmail.com>
2016-05-03 08:36:35 +0000

    tests written

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const main = require('../main.js');
+
+describe('Datascraper', function() {
+  describe('getLogs()', function () {
+    // pending test below
+    it('should return a https response with the status code: 200');
+    it('should return a JSON object');
+    it('should return a csv object');
+    it('should return a output a csv file to output/');
+  });
+});
