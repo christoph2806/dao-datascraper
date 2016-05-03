5d4468da2f53bb673b5c7eb4b694bf8ba43b0c5d
Peter C <peter.chau.esq@gmail.com>
2016-05-03 08:48:39 +0000

    commented code

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 "use strict";
 
+// require npm packages
 const bl = require('bl');
 const fs = require('fs');
 const https = require('https');
 const json2csv = require('json2csv');
 
+// set up variables
+// DAO Ethereum address
 const address = '0xbb9bc244d798123fde783fcc1c72d3bb8c189413';
 const date = new Date();
-const file = `output/${date}.csv`;
+// path/to/csv
+const output = `output/${date}.csv`;
+// csv field headers
 const fields = ['blockNumber', 'timeStamp', 'hash', 'nonce', 'blockHash', 'transactionIndex', 'from', 'to', 'value', 'gas', 'gasPrice', 'input', 'contractAddress', 'cumulativeGasUsed', 'gasUsed', 'confirmations'];
 
 process.stdout.write('DAO-datascraper initialized!\n');
 
 // get data from the etherscan.io api https://etherscan.io/apis
+// Make https request
 https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`, (res) => {
 
     process.stdout.write(`StatusCode: ${res.statusCode}\n`);
 
+    // return a String not an Buffer
     res.setEncoding('utf8');
-
+    // wait for and combine all responses
     res.pipe(bl((err, data) => {
 
         if (err) {
@@ -27,8 +34,10 @@ https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${a
 
         process.stdout.write('Getting DAO transaction logs from etherscan\n');
 
+        // convert to JSON
         const json = JSON.parse(data);
 
+        // convert to csv
         json2csv({
             data: json["result"],
             fields: fields
@@ -38,13 +47,14 @@ https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${a
                 process.stdout.write(`Error: ${err}\n`);
             }
 
-            fs.writeFile(file, csv, (err) => {
+            // export csv to output/
+            fs.writeFile(output, csv, (err) => {
 
                 if (err) {
                     process.stdout.write(`Error: ${err}\n`);
                 }
 
-                process.stdout.write(`File saved: ${file}\n`);
+                process.stdout.write(`File saved: ${output}\n`);
 
             });
 
--- test/test.js
+++ test/test.js
@@ -1,13 +1,16 @@
+// require npm packages
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+const request = require('request');
+
+// require code to test
 const main = require('../main.js');
 
 describe('Datascraper', function() {
-  describe('getLogs()', function () {
     // pending test below
     it('should return a https response with the status code: 200');
+    it('should return a String');
     it('should return a JSON object');
     it('should return a csv object');
     it('should return a output a csv file to output/');
-  });
 });
